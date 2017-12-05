637738c0c66aca2c59c6db97f8239949294f10a9
andy <lehner.andreas@gmail.com>
2017-12-05 22:16:10 +0100

    implementation of cached matrix module

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+#' @description This module holds a function to make a matrix of which the inverse calculation can be cached.
+#' @details A matrix has to be created using the @seealso makeCacheMatrix function
+#'          and the matrix inversion can be retrieved with the @seealso cacheSolve function.
+#' @author Andreas Lehner
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#' @description Create a new matrix which is cacheable
+#' @param x The input matrix which will be made cachable
+makeCacheMatrix <- function(x = matrix())
+{
+    # cached inversion
+    cachedInvertedMatrix <- NULL
+    
+    # set a matrix
+    set <- function(newMatrix)
+    {
+        x <<- newMatrix
+        cachedInvertedMatrix <<- NULL
+    }
+    
+    # return the matrix
+    get <- function() return(x)
+    
+    # set the inverted matrix
+    setInversion <- function(inversion) cachedInvertedMatrix <<- inversion
+    
+    # return the inverted matrix
+    getinversion <- function() return(cachedInvertedMatrix)
+    
+    # return a list containing all the functions
+    return(list(set = set, get = get, setInversion = setInversion, getinversion = getinversion))
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#' @description Solves the inverse of the matrix.
+#' @param x The input cacheable input matrix created with @seealso makeCacheMatrix
+#' @return The inverse of the matrix x
+cacheSolve <- function(x, ...)
+{
+    # get the current inversion
+    inv <- x$getinversion()
+    
+    # check if a cached value is available
+    if (!is.null(inv))
+    {
+        # if the cached value is available, return it
+        return(inv)
+    }
+    
+    # solve the matrix if it is not available
+    inv <- solve(x$get(), ...)
+    
+    # set the inversion to make the cached value available for next use
+    x$setInversion(inv)
+    
+    # return the inversion
+    return(inv)
 }
